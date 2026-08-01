5203bd4ffef4acd986a2dc9248740b4232e7ebe6
Hofmann250 <johan.hofmann250@gmail.com>
2026-08-02 09:51:19 +1000

    modify snippetView with id users

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +15,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a spicific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
