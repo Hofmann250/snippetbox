e178e95ce1bb2d7e174e115fe62cf4940360ef96
Hofmann250 <johan.hofmann250@gmail.com>
2026-08-01 17:34:48 +1000

    create first draft of homepage, snippetView and snippetCreate

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Snippetbox"))
+}
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a spicific snippet..."))
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", home)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
