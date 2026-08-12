f510cb5f8f38d652c5b4f9ba76b95aacfb627a39
Hofmann250 <johan.hofmann250@gmail.com>
2026-08-12 10:40:30 +1000

    add database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,15 +17,39 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn:=flag.String("dsn","web:pass@/snippetbox?parseTime=true","MySQL data source name")
+	
 	flag.Parse()
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	app := &application{
-		logger: logger,
+	infoLog:=log.New(os.Stdout,"INFO\t",log.Ldate|log.Ltime)
+	errorLog:=log.New(os.Stderr,"ERROR\t",log.Ldate|log.Ltime|log.Lshortfile)
+	
+	db,err:=openDB(*dsn)
+	if err!=nil{
+		errorLog.Fatal(err)
 	}
+	defer db.Close()
+	app := &application{
+		errorLog:errorLog,
+		infoLog:infoLog,
+	}
+	srv:=&http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
+	infoLog.Printf("Starting server on %s",*addr)
+	err =srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
 
-	logger.Info("Starting server on ", "addr", *addr)
-	err := http.ListenAndServe(*addr, app.routes())
-	logger.Error(err.Error())
-	os.Exit(1)
+func openDB(dsn string) (*sql.DB, error){
+db,err:=sql.Open("mysql",dsn)
+if err!=nil{
+	return nil, err
+}
+if err=db.Ping(); err!=nil{
+	return nil,err
+}
+return db, nil
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,6 +6,7 @@ import (
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
+	
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
@@ -13,5 +14,6 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
 	return mux
 }
