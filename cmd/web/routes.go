41fb7eb3e8665c506b76fbe9321c6682823ebbe7
Hofmann250 <johan.hofmann250@gmail.com>
2026-09-05 20:28:08 +1000

    add using request context

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,7 +12,7 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	dinamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dinamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	mux.Handle("GET /{$}", dinamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dinamic.ThenFunc(app.snippetView))
